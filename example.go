2f89b232cb75232c4867f6658283e9173f725199
Greg Szalay <gr.szalay@gmail.com>
2022-09-19 08:40:00 +0200

    Added pretty print to examples for clarity

--- example.go
+++ example.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 
 	"github.com/gregszalay/ocpp-go/types/BootNotificationRequest"
+	"github.com/sanity-io/litter"
 )
 
-func main() {
-
+func createExampleJSONString() string {
 	// Create example OCPP message object (e.g. BootNotificationRequest)
 	boot_req := BootNotificationRequest.BootNotificationRequestJson{
 		Reason: "Unknown",
@@ -19,20 +19,32 @@ func main() {
 	}
 
 	// Create JSON string from example object
-	fmt.Println("Marshalling example message...")
-	boot_req_json_string, marshal_err := json.Marshal(boot_req)
+	boot_req_json_string, marshal_err := json.MarshalIndent(boot_req, "", "  ")
 	if marshal_err != nil {
 		fmt.Printf("Failed OCPP message json marshal. Error: %s", marshal_err)
 	}
-	fmt.Printf("Example OCPP message successfully marshalled.\nJSON:\n%s\n", boot_req_json_string)
+	return string(boot_req_json_string)
+}
+
+func main() {
+
+	example_message := createExampleJSONString()
+	fmt.Println("*******************************")
+	fmt.Println("Example OCPP message json:")
+	fmt.Println("*******************************")
+	fmt.Printf("%s\n", example_message)
+	fmt.Println("*******************************")
 
 	// Unmarshal JSON string to see the types in action
-	fmt.Println("Unmarshalling example message...")
 	var req BootNotificationRequest.BootNotificationRequestJson
-	unmarshal_err := req.UnmarshalJSON([]byte(boot_req_json_string))
+	unmarshal_err := req.UnmarshalJSON([]byte(example_message))
 	if unmarshal_err != nil {
 		fmt.Printf("Failed OCPP message json unmarshal. Error: %s", unmarshal_err)
+	} else {
+		fmt.Println("Example message unmarshalled using the types:")
+		fmt.Println("*******************************")
+		litter.Dump(req)
 	}
-	fmt.Printf("Example OCPP message successfully unmarshalled uding the types.\nObject:\n%+v\n", req)
+	fmt.Println("*******************************")
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gregszalay/ocpp-go
 
 go 1.18
+
+require github.com/sanity-io/litter v1.5.5 // indirect
